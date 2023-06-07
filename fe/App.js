22c250a1955d3ab548eb6b79d045d4c439d4c0c0
Nurlat Bekdullayev <natuspati@gmail.com>
2023-06-07 22:44:31 +0600

    Finish FE logic for the game. Override Bootstrap using SASS. Enable serving static/media files in DEBUG.

--- fe/App.js
+++ fe/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
-import {Configuration, CountryExists, GameApi} from "../api_client";
+import {Configuration, GameApi} from "../api_client";
 import Cookies from 'js-cookie';
 
 const GameApp = function (props) {
@@ -8,11 +8,11 @@ const GameApp = function (props) {
     const inputRef = useRef() // autofocus to input field
 
     // Game state variables
+    const maxTries = 4;
     const [finishedToday, setFinishedToday] = useState(false);
     const [gameState, setGameState] = useState("wait");
-    const [numTries, setNumTries] = useState(4);
-    const [triedCountries, setTriedCountries] = useState(["Very Long String Very", "test", "string", "test", "test"]);
-    const [nonexistentCountry, setNonexistentCountry] = useState("test");
+    const [numTries, setNumTries] = useState(maxTries);
+    const [triedCountries, setTriedCountries] = useState([]);
     const [guess, setGuess] = useState("");
     const [lastValidGuess, setLastValidGuess] = useState("")
 
@@ -26,6 +26,7 @@ const GameApp = function (props) {
     const [region, setRegion] = useState("");
     const [population, setPopulation] = useState(0);
     const [flag, setFlag] = useState("");
+    const [country, setCountry] = useState({});
 
     const client = new GameApi(new Configuration({
         basePath: 'http://127.0.0.1:8000',
@@ -44,6 +45,14 @@ const GameApp = function (props) {
     // Retrieve country information
     useEffect(() => {
         client.gameCountryTodayRetrieve().then((result) => {
+            setCountry({
+                "name": result.name,
+                "capital": result.capital,
+                "region": result.region,
+                "population": result.population,
+                "flag": result.flag
+            });
+
             setName(result.name);
             setCapital(result.capital);
             setRegion(result.region);
@@ -53,28 +62,46 @@ const GameApp = function (props) {
         });
     }, []);
 
+    // Disable input if the game ends
+    useEffect(() => {
+        if (gameState === "fail" || gameState === "success") {
+            setInputDisabled(true);
+        }
+    }, [gameState]);
+
     const handleSubmit = (event) => {
         event.preventDefault();
         setLastValidGuess(guess);
 
-        // Check if the submitted guess is correct
-        if (guess.toLowerCase() === name.toLowerCase()) {
-            setGuess(name);
-            setGameState("success");
-            setInputDisabled(true);
+        // Check if the guess has already been tried
+        if (triedCountries.includes(titleCase(guess))) {
+            setGameState("repeat")
         } else {
-            client.gameCountryCheckCreate({
-                'countryExists': {'name': guess}
-            }).then((result) => {
-                if (result._exists) {
-                    setGameState("wait")
-                    setNumTries(x => x - 1);
-                    setTriedCountries(countries => [...countries, result.name]);
-                } else {
-                    setGameState("skip")
-                    setNonexistentCountry(x => result.name);
-                }
-            });
+            // Check if the guess is correct
+            if (guess.toLowerCase() === name.toLowerCase()) {
+                setGuess(name);
+                setGameState("success");
+            } else {
+                // Check if the guess is valid
+                client.gameCountryCheckCreate({
+                    'countryExists': {'name': guess}
+                }).then((result) => {
+                    // Deduct a try for incorrect guess
+                    if (result._exists) {
+                        setNumTries(x => x - 1);
+                        // Because setState updates asynchronously, use 1 as min value of tries
+                        if (numTries > 1) {
+                            setGameState("wait");
+                        } else {
+                            setGameState("fail");
+                        }
+                        setTriedCountries(countries => [...countries, titleCase(result.name)]);
+                    } else {
+                        // Don't deduct for incorrect guess
+                        setGameState("skip")
+                    }
+                });
+            }
         }
 
         // Show alert after submission
@@ -83,15 +110,6 @@ const GameApp = function (props) {
         // Reset input box value and focus to it
         event.target.reset();
         inputRef.current?.focus();
-
-        // Autoclose the alert
-        // const timeId = setTimeout(() => {
-        //     setShowAlert(false);
-        // }, 10000);
-        // return () => {
-        //     clearTimeout(timeId)
-        // }
-
     };
 
     const alertFormatter = (responseGameState, responseNameNoFormat) => {
@@ -108,7 +126,10 @@ const GameApp = function (props) {
                 out = ["danger", name, " is correct answer."];
                 break;
             case "skip":
-                out = ["secondary", responseName, " is not a country."]
+                out = ["secondary", responseName, " is not a country."];
+                break;
+            case "repeat":
+                out = ["secondary", responseName, " has already been tried."];
                 break;
             default:
                 out = ["primary", "Debug", " message"]
@@ -121,60 +142,157 @@ const GameApp = function (props) {
             <div className="col-10 col-lg-6 mx-auto">
                 <div className="row">
                     <div className="col">
-                        Remaining tries: {numTries}
+                        <div className="custom-progress my-2 my-lg-3">
+                            <ProgressBar numTries={numTries} maxTries={maxTries}/>
+                        </div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col">
                         <form onSubmit={handleSubmit}>
                             <fieldset disabled={inputDisabled}>
-                                <label>
-                                    <input type="text" className="form-control"
-                                           placeholder="Enter country name" autoComplete="off" ref={inputRef}
-                                           onChange={(e) => setGuess(e.target.value)}
+                                <div className="input-group w-auto">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Enter country name"
+                                        aria-label="Enter country name"
+                                        aria-describedby="submitButton"
+                                        autoComplete="off"
+                                        ref={inputRef}
+                                        onChange={(e) => setGuess(e.target.value)}
                                     />
-                                </label>
-                                <button disabled={guess === ""}>Submit</button>
+                                    <button disabled={guess === ""} className="btn btn-primary" type="submit"
+                                            id="submit-button" data-mdb-ripple-color="dark">
+                                        Enter
+                                    </button>
+                                </div>
                             </fieldset>
                         </form>
                     </div>
-                    <div className="row">
-                        <div className="col">
-                            <Message args={alertFormatter(gameState, lastValidGuess)}
-                                     show={showAlert}/>
-                        </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <Message args={alertFormatter(gameState, lastValidGuess)}
+                                 show={showAlert}/>
                     </div>
-                    <div className="row row-cols-2 row-cols-lg-4 g-2 g-lg-4">
-                        {triedCountries.map((name, i) => {
-                            return (
-                                <div className="col my-auto py-1 pb-lg-2" key={i}>
-                                    <div className="border my-auto" key={i}>
-                                        {name}
-                                    </div>
+                </div>
+                <div className="row row-cols-2 row-cols-lg-4 tried-countries">
+                    {triedCountries.map((name, i) => {
+                        return (
+                            <div className="col my-auto py-1 pb-lg-2" key={i}>
+                                <div className="border my-auto" key={i}>
+                                    {name}
                                 </div>
-                            )
-                        })}
-                    </div>
-
-
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="row">
+                    <AnswerForm showTries={maxTries - numTries + 1} country={country} showFinal={gameState}/>
                 </div>
             </div>
         </div>
     );
 };
 
-const Message = ({
-                     args, show
-                 }) => {
+const Message = ({args, show}) => {
     if (show) {
         return (
-            <div className={`alert alert-${args[0]}`}>
+            <div className={"alert alert-" + args[0] + " my-2 my-lg-3"}>
                 <strong>{args[1]}</strong> {args[2]}
             </div>
         )
     }
 
 }
+
+const ProgressBar = (props) => {
+    const {numTries, maxTries} = props;
+    const completed = numTries / maxTries * 100;
+    let labelText;
+
+    switch (numTries) {
+        case 0:
+            labelText = "";
+            break;
+        case 1:
+            labelText = `Last try!`;
+            break;
+        default:
+            labelText = `${numTries} tries left`;
+    }
+
+    const fillerStyles = {
+        width: `${completed}%`,
+    };
+
+    return (
+        <div className="progress-container">
+            <div style={fillerStyles} className="progress-filler">
+                <span className="progress-label">{labelText}</span>
+            </div>
+        </div>
+    );
+};
+
+const AnswerForm = (props) => {
+    const {showTries, country, showFinal} = props;
+    let shownNumberFields = 0;
+    if (showFinal === "fail" || showFinal === "success") {
+        shownNumberFields = 5;
+    } else {
+        delete country.name;
+        shownNumberFields = showTries;
+    }
+
+    let formattedCountry = {};
+    for (const [index, [key, value]] of Object.entries(Object.entries(country))) {
+        if (Number(index) === shownNumberFields) {
+            break;
+        }
+        formattedCountry[key] = value;
+    }
+
+    return (
+        <div> {
+            Object.keys(formattedCountry).map((countryKey, index) => {
+                return (<div key={index} className="col mx-auto">
+                    <AnswerFormTagFormatter countryKey={countryKey} countryVal={formattedCountry[countryKey]}/>
+                </div>)
+            })
+        }</div>
+    );
+};
+
+const AnswerFormTagFormatter = (props) => {
+    const {countryKey, countryVal} = props;
+    let formattedCountryVal = "";
+
+    if (typeof countryVal == 'number') {
+        formattedCountryVal = countryVal.toLocaleString();
+    } else {
+        formattedCountryVal = countryVal;
+    }
+
+    if (countryKey === 'flag') {
+        return <div className="row justify-content-center my-1">
+            <div className="col col-6 col-lg-4">
+                <img src={countryVal["full_size"]} alt="Flag" width="100%" border="1"/>
+            </div>
+        </div>
+    } else {
+        return <div className="row justify-content-center my-1">
+            <div className="col col-6 col-lg-4 border-bottom ">
+                {titleCase(countryKey)}: {formattedCountryVal}
+            </div>
+        </div>
+    }
+
+};
+
+export default ProgressBar;
+
 
 function titleCase(str) {
     str = str.toLowerCase().split(' ');
